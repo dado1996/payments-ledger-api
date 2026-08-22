63f1124c9966259a53174cd710a571b3ae717ea3
Diego Delgado <diego.960705@gmail.com>
2026-08-22 18:44:03 -0500

    feat: create transfer errors

--- src/domain/errors.ts
+++ src/domain/errors.ts
@@ -1,10 +1,12 @@
-import type { Money } from "./money/money.js";
+import type { Currency, Money } from "./money/money.js";
 import type { AccountId } from "./shared/shared.js";
 
 export enum ErrorCodes {
   UNBALANCED_TRANSACTION = "UNBALANCED_TRANSACTION",
   INVALID_ENTRY = "INVALID_ENTRY",
   DUPLICATE_TRANSFER = "DUPLICATE_TRANSFER",
+  ACCOUNT_NOT_FOUND = "ACCOUNT_NOT_FOUND",
+  CURRENCY_MISMATCH = "CURRENCY_MISMATCH",
 }
 
 export abstract class DomainError extends Error {
@@ -35,9 +37,25 @@ export class InvalidEntryError extends DomainError {
 }
 
 export class IdempotencyConflictError extends DomainError {
-  public readonly idempotencyKey: string | undefined;
-  constructor(message: string, idempotencyKey?: string) {
+  public readonly idempotencyKey: string;
+  constructor(message: string, idempotencyKey: string) {
     super(message, ErrorCodes.DUPLICATE_TRANSFER);
     this.idempotencyKey = idempotencyKey;
+  }
+}
+
+export class AccountNotFoundError extends DomainError {
+  public readonly id: string | undefined;
+  constructor(message: string, id?: string) {
+    super(message, ErrorCodes.ACCOUNT_NOT_FOUND);
+    this.id = id;
+  }
+}
+
+export class CurrencyMismatchError extends DomainError {
+  public readonly currency: Currency | undefined;
+  constructor(message: string, currency?: Currency) {
+    super(message, ErrorCodes.CURRENCY_MISMATCH);
+    this.currency = currency;
   }
 }
